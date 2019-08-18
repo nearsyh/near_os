18c8bd74c7b8c8824fb4bbef9b1f9f0a9a1409a1
Near <nearsy.h@gmail.com>
2019-08-18 21:56:00 +0800

    Add page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -11,16 +11,22 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         // Create an empty IDT
         let mut idt = InterruptDescriptorTable::new();
+
+        // Exception handler
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
+        // Hardware interrupts handlers
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
         idt
     };
 }
@@ -38,6 +44,20 @@ extern "x86-interrupt" fn breakpoint_handler(
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: &mut InterruptStackFrame, _error_code: u64) {
     println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    hlt_loop();
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
     hlt_loop();
 }
 
